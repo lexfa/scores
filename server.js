24566c05c1c505a171fcad1682f310b3295ffd72
yacaFx <sergiobritor@gmail.com>
2014-03-13 03:34:41 -0600

    Added send scores function

--- server.js
+++ server.js
@@ -12,16 +12,35 @@ server.listen(port);
 console.log('http server listening on %d', port);
 
 var wss = new WebSocketServer({server: server});
+
+
 console.log('websocket server created');
+
 wss.on('connection', function(ws) {
-    var id = setInterval(function() {
-        ws.send(JSON.stringify(new Date()), function() {  });
-    }, 1000);
+    ws.on('message', function(message) {
+        console.log('received: %s', message);
+        //ws.send(message);
+         wss.broadcast(message);
+    });
 
-    console.log('websocket connection open');
+    //ws.send(message);
+});
 
-    ws.on('close', function() {
+
+wss.broadcast = function(data) {
+  for (var i in this.clients)
+    this.clients[i].send(data);
+};
+
+/*
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
+    wss.broadcast('Broadcast message: ' + message);
+  });
+
+   ws.on('close', function() {
         console.log('websocket connection close');
         clearInterval(id);
     });
 });
+*/
